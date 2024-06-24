93f300cd543e54d60f5471e7cda5a9b7aacb68ab
Porter Young <porteryoung2005@gmail.com>
2024-06-24 15:24:43 -0600

    made html stub

--- app/services/QuestionsService.js
+++ app/services/QuestionsService.js
@@ -9,12 +9,12 @@ class QuestionsService {
 
     async getQuestions() {
         // @ts-ignore
-        const response = await axios.get('https://opentdb.com/api.php?amount=10&category=30&difficulty=easy&type=multiple')
-        let questionsReceived = response.data.results;
-        questionsReceived.forEach((question) => {
-            console.log('questions printing');
-            let newQuestions = new Question(question)
-        });
+        // const response = await axios.get('https://opentdb.com/api.php?amount=10&category=30&difficulty=easy&type=multiple')
+        // let questionsReceived = response.data.results;
+        // questionsReceived.forEach((question) => {
+        //     console.log('questions printing');
+        //     let newQuestions = new Question(question)
+        // });
     }
 }
 export const questionsService = new QuestionsService
